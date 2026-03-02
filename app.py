8f7c0a4339f303482bed3c52a5372a0a1519b25d
dputar <calimerko@gmail.com>
2026-03-02 20:09:37 +0100

    godišnjis2d233435454

--- app.py
+++ app.py
@@ -991,12 +991,6 @@ else:
     elif st.session_state.stranica == "dokumenti":
         st.title("🏖️ Godišnji odmor i slobodni dani")
 
-        # Inicijaliziraj session_state za privremeni unos i reset forme
-        if "temp_odmor" not in st.session_state:
-            st.session_state.temp_odmor = None
-        if "form_reset" not in st.session_state:
-            st.session_state.form_reset = False
-
         # Ručno definirani hrvatski praznici i blagdani za 2026-2040 (koristi date objekat)
         holidays_dict = {
             2026: [date(2026, 1, 1), date(2026, 1, 6), date(2026, 4, 5), date(2026, 4, 6), date(2026, 5, 1), date(2026, 5, 30), date(2026, 6, 22), date(2026, 8, 15), date(2026, 11, 1), date(2026, 11, 18), date(2026, 12, 25), date(2026, 12, 26)],
