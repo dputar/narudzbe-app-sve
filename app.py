b365e52fee04f45270286e95d36af27b3af164d8
dputar <calimerko@gmail.com>
2026-03-04 20:45:12 +0100

    Dodana slika template za PDF222223

--- app.py
+++ app.py
@@ -1479,9 +1479,9 @@ else:
                                 # Podešene koordinate za centriranje teksta na točkicama (prilagodio prema slici)
                                 # y je od dna stranice (0 dolje, height gore)
                                 c.drawCentredString(width / 2 - 45*mm, height - 129*mm, ime_prezime)  # ime centrirano
-                                c.drawCentredString(width / 2 - 5*mm, height - 145*mm, broj_dana)  # broj dana centrirano
+                                c.drawCentredString(width / 2 - 5*mm, height - 144*mm, broj_dana)  # broj dana centrirano
                                 c.drawCentredString(width / 2 - 7*mm, height - 162*mm, datum_od)  # datum od centrirano
-                                c.drawCentredString(width / 2 + 50*mm, height - 180*mm, datum_do)  # datum do centrirano
+                                c.drawCentredString(width / 2 - 55*mm, height - 185*mm, datum_do)  # datum do centrirano
                                 c.drawCentredString(width / 2, height - 200*mm, prvi_radni_dan)  # prvi radni dan centrirano
                                 c.drawCentredString(width / 2, height - 220*mm, datum_podnosenja)  # datum podnošenja centrirano
 
