bed43eb0702a87d3925786f452e2d6e62185b068
dputar <calimerko@gmail.com>
2026-03-05 18:37:33 +0100

    Login33443

--- app.py
+++ app.py
@@ -204,7 +204,7 @@ if time.time() - st.session_state.last_refresh_time > 300:
     # ────────────────────────────────────────────────
     # NARUDŽBE – pregled
     # ────────────────────────────────────────────────
-    elif st.session_state.stranica == "narudzbe":
+    elif st.session_state.stranica == "narudžbe":
         st.title("Pregled narudžbi")
 
         col1, col2 = st.columns([6, 4])
